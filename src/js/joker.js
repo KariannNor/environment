16b9c11db3a97085bb3ced08fbd3dc919ca1cdf0
Kariann Norheim <kariann.norheim@gmail.com>
2024-03-14 09:41:00 -0400

    test of lintfix

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,5 +1,13 @@
-import joker from "give-me-a-joke";
+import joker from 'give-me-a-joke';
 
-// import "joker-karianns";
-
-joker.getRandomDadJoke(console.log);
+export function dadJoke() {
+	return new Promise((resolve, reject) => {
+		try {
+			joker.getRandomDadJoke(joke => {
+				resolve(joke);
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
